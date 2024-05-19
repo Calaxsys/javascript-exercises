9441dfb9bec4720c84f31f773aa5a72f3cf26eed
Chris <ffson1999@yahoo.com>
2024-05-19 08:30:55 -0400

    Finished javascript exerces 03_reverseString, solved by splitting string, and storing that into an array, then using .reverse() on the array, and lastly .join() on that reversed array.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reversedStringArray = splitString.reverse();
+    let reversedString = reversedStringArray.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
