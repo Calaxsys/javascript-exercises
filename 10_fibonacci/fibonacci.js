15fa898334f05b2d90a6ee9330a81b1cdf523a20
Chris <ffson1999@yahoo.com>
2024-05-22 06:30:40 -0400

    Completed javascript exercises 8 through 12, need to redo again to instill information.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    let count;
+    if (typeof countArg !== "number") {
+        count = parseInt(countArg);
+    } else {
+        count = countArg;
+    }
 
+    if (count < 0) return "OOPS"
+    if (count == 0) return 0;
+
+    let firstPrevious = 1;
+    let secondPrevious = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrevious + secondPrevious;
+        secondPrevious = firstPrevious;
+        firstPrevious = current;
+    }
+
+    return firstPrevious;
 };
 
 // Do not edit below this line
